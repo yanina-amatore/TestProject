946901308dc5be6b826c963f156aa317a95549e2
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-05-09 10:12:42 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*int[] inventory = {200,450,700,175,250};
+
+int sum = 0;
+int bin=0;
+
+foreach (int items in inventory)
+{
+  sum+=items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+};
+
+Console.WriteLine($"We have {sum} items in inventory.");*/
+
+
+string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string name in orderIDs)
+{
+  if ( name.StartsWith('B'))
+  {
+    Console.WriteLine($"The name {name} starts with 'B'");
+  }
+
+}
