624726f4807287b8631aecae5491e6f6d185d5b7
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-05-09 10:29:47 +1200

    clean up commit

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ foreach (int items in inventory)
 
 Console.WriteLine($"We have {sum} items in inventory.");*/
 
-
+/*
 string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
 
 foreach (string name in orderIDs)
@@ -22,4 +22,22 @@ foreach (string name in orderIDs)
     Console.WriteLine($"The name {name} starts with 'B'");
   }
 
+}*/
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+
+    int prefixValue = random.Next(65, 70);  
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+ 
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
