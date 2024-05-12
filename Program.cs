30c7f0059cfff6ca1c384543258afb4d8370734d
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-05-13 11:02:57 +1200

    fizzbuzz challenge

--- Program.cs
+++ Program.cs
@@ -1,64 +1,52 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0])
+﻿
+/*for (int i = 0; i < 10; i++) 
 {
-    case "01":
-          type = "Sweat shirt";
-        break;
+  Console.WriteLine(i);
+      if (i == 7) break;
+}; */
 
-    case "02":
-          type = "T-Shirt";;
-        break;
 
-    case "03":
-        type = "Sweat pants";
-        break;
+/*string[] names = { "Alex", "Eddie", "David", "Michael" };
 
-    default:
-      type = "Other";
-      break;
-}
-
-switch (product[1])
+for (int i = 0 ; i <= names.Length - 1; i++)
 {
-    case "BL":
-         color = "Black";
-        break;
+    Console.WriteLine(names[i]);
+};
 
-    case "MN":
-           color = "Maroon";
-        break;
+foreach (string name in names){
+  if( name == "David" ) name = "Sammy";
+};
 
-    default:
-        color = "White";
-        break;
-}
+for ( int i = 0; i <= names.Length -1; i++){
+  if( names[i] == "David" ) names[i] = "Sammy";
+  Console.WriteLine(names[i]);
+}; */
 
-switch (product[2])
+/* FizzBuzz challenge
+
+- Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+- When the current value is divisible by 3, print the term Fizz next to the number.
+- When the current value is divisible by 5, print the term Buzz next to the number.
+- When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+
+*/
+for (int i = 1; i <= 100; i++)
 {
-    case "S":
-        size = "Small";
-        break;
-
-    case "M":
-           size = "Medium";
-        break;
-    case "L":
-           size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
-
-
-
-Console.WriteLine($"Product: {size} {color} {type}");
+  if (i % 3 == 0 && i % 5 == 0)
+  {
+    Console.WriteLine(i + " FizzBuzz");
+  }
+  else if (i % 3 == 0)
+  {
+    Console.WriteLine(i + " Fizz");
+  }
+  else if (i % 5 == 0)
+  {
+    Console.WriteLine(i + " Buzz");
+  }
+  else
+  {
+    Console.WriteLine(i);
+  }
+};
+  
