4811abccc8b9312fd7196d68e6e390168a7da569
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-05-13 12:02:24 +1200

    Monster Hero challenge do while

--- Program.cs
+++ Program.cs
@@ -1,26 +1,50 @@
-﻿
-Random random = new Random();
+﻿// turn off copilot mode
+// turn off copilot mode
 
-int current = random.Next(1, 11);
+ /*int heroHealth = 10 ;
+int monsterHealth = 10 ;
 
-/*do{
+Random diceAttacl = new Random();
 
-  current =  random.Next(1, 11);
-  Console.WriteLine( current );
-} while (current !=7); 
-
-while (current >= 3)
-{
-    Console.WriteLine(current);
-    current = random.Next(1, 11);
-}
-Console.WriteLine($"Last number: {current}"); */
 
 do
 {
-    current = random.Next(1, 11);
+  int  diceRoll = diceAttack.Next(1, 11);
+  monsterHealth -= diceRoll;
+  if (monsterHealth >0){
+    Console.WriteLine($"Monster was damaged and lost {diceRoll} health and now has {monsterHealth} health.") ;
+    int rollAgain = diceAttack.Next(1, 11);
+    heroHealth -= rollAgain;
+    if (heroHealth >0){
+      Console.WriteLine($"Hero was damaged and lost {rollAgain} health and now has {heroHealth} health.") ;
+    }
+    else{
+      Console.WriteLine($"Hero has been defeated and lost and lost {rollAgain} remaining. Monster Wins!") ;
+      break ;
+    }
+  }else{
+      Console.WriteLine($"Hero wins. Monsters has been defeated. he lost {monsterHealth} remaining");
+    }
 
-    if (current >= 8) continue;
+} while (monsterHealth >0); */
 
-    Console.WriteLine(current);
-} while (current != 7);
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c7f0059cfff6ca1c384543258afb4d8370734d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f318181d9e9e3e4a096484dec0c48506a229f269")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
